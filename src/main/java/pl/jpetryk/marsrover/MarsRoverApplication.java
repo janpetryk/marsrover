cbddffdb920cfdbfbc5833eae80a4aab6d5d5c2f
Jan Petryk <janpetryk@gravity9solutions.com>
2021-07-20 11:57:05 +0200

    moved input parser to a separate class

--- src/main/java/pl/jpetryk/marsrover/MarsRoverApplication.java
+++ src/main/java/pl/jpetryk/marsrover/MarsRoverApplication.java
@@ -1,98 +1,38 @@
 package pl.jpetryk.marsrover;
 
-import static java.lang.System.lineSeparator;
-
 import java.io.PrintStream;
-import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 public class MarsRoverApplication {
 
-  private final Scanner scanner;
+  private final InputParser inputParser;
   private final PrintStream printStream;
 
-  public MarsRoverApplication(Scanner scanner, PrintStream printStream) {
-    this.scanner = scanner;
+  public MarsRoverApplication(InputParser inputParser, PrintStream printStream) {
+    this.inputParser = inputParser;
     this.printStream = printStream;
   }
 
   public static void main(String[] args) {
-    var application = new MarsRoverApplication(new Scanner(System.in), System.out);
+    PrintStream printStream = System.out;
+    Scanner scanner = new Scanner(System.in);
+    var inputParser = new InputParser(scanner, printStream);
+    var application = new MarsRoverApplication(inputParser, printStream);
     application.run();
   }
 
   void run() {
-    int x = parseInt("Enter X coordinate:");
-    int y = parseInt("Enter Y coordinate:");
-    Direction direction = parseDirection();
+    int x = inputParser.parseInt("Enter X coordinate:");
+    int y = inputParser.parseInt("Enter Y coordinate:");
+    Direction direction = inputParser.parseDirection();
 
     Rover kata = new Rover(new Point(x, y), direction);
 
-    List<Consumer<Rover>> parsedCommands = parseCommands();
+    List<Consumer<Rover>> parsedCommands = inputParser.parseCommands();
     parsedCommands.forEach(action -> action.accept(kata));
-    printWithNewLine(kata.report());
-  }
-
-  private int parseInt(String message) {
-    Integer result = null;
-
-    while (result == null) {
-      printWithNewLine(message);
-      try {
-        result = scanner.nextInt();
-      } catch (NoSuchElementException e) {
-        scanner.nextLine();
-        printWithNewLine("Value has to be an integer.");
-      }
-    }
-    return result;
-  }
-
-
-  private Direction parseDirection() {
-    Direction result = null;
-
-    while (result == null) {
-      printWithNewLine("Enter direction robot is facing (NORTH, SOUTH, EAST, WEST):");
-      try {
-        result = Direction.valueOf(scanner.next());
-      } catch (IllegalArgumentException e) {
-        scanner.nextLine();
-        printWithNewLine("Value has to be an of NORTH, SOUTH, EAST, WEST.");
-      }
-    }
-
-    return result;
-  }
-
-  private List<Consumer<Rover>> parseCommands() {
-    List<Consumer<Rover>> result = null;
-
-    while (result == null) {
-      printWithNewLine("Enter commands (F, B, L, R):");
-      try {
-
-        String commands = scanner.next();
-        result = Arrays.stream(commands.split(""))
-            .map(String::toUpperCase)
-            .map(RoverCommand::valueOf)
-            .map(RoverCommand::getAction)
-            .collect(Collectors.toList());
-      } catch (IllegalArgumentException e) {
-        scanner.nextLine();
-        printWithNewLine("Value has to be an of (F, B, L, R).");
-      }
-    }
-    return result;
-  }
-
-  private void printWithNewLine(String s) {
-    printStream.print(s);
-    printStream.print(lineSeparator());
+    printStream.print(kata.report());
   }
 
 }
